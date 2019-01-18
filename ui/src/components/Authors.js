ac1df8bfcd02f117f43ab72ed7fa706a4c0926dd
Adrian Bednarz <adrianbednarz@gmail.com>
2019-01-18 15:46:09 +0100

    Github ref

--- ui/src/components/Authors.js
+++ ui/src/components/Authors.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Authors = () => (
   // todo: or Janusze-AI?
-  <h4 className="text-center janusze-font-style-large" style={{position: "fixed", right: "5vw", bottom: 0}}>Adrian Bednarz & Rafał Kobiela &copy; 2018</h4>
+  <h4 className="text-center janusze-font-style-large" style={{position: "fixed", right: "5vw", bottom: 0}}>Adrian Bednarz & Rafał Kobiela &copy; 2019 <a href="https://github.com/GreenShade/similar-faces">GitHub</a></h4>
 );
 
 export default Authors;
